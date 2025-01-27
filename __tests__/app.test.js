088f7adaf5803919dbaed9b487d62a6e410e2995
Patrick-jc-Evans <paddy@paddyevans.com>
2025-01-27 11:16:41 +0000

    create endpoint /api

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,15 @@
 const endpointsJson = require("../endpoints.json")
 /* Set up your test imports here */
+const request = require("supertest")
+const app = require("../app/app.js")
 
 /* Set up your beforeEach & afterAll functions here */
 
-xdescribe("GET /api", () => {
+afterAll(() => {
+    app.close()
+})
+
+describe("GET /api", () => {
     test("200: Responds with an object detailing the documentation for each endpoint", () => {
         return request(app)
             .get("/api")
