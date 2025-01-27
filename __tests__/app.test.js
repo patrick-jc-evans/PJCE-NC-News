7a3d769dcc3d1d061cea98dab99c7bf729737b2b
Patrick-jc-Evans <paddy@paddyevans.com>
2025-01-27 14:39:27 +0000

    add get api/articles/articleId endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,7 +24,7 @@ describe("GET /api", () => {
 })
 
 describe("GET /api/topics", () => {
-    test("200 Responds with an array of topics, each with a slug and description", () => {
+    test("200: Responds with an array of topics, each with a slug and description", () => {
         return request(app)
             .get("/api/topics")
             .expect(200)
@@ -37,6 +37,47 @@ describe("GET /api/topics", () => {
                     { slug: "cats", description: "Not dogs" },
                     { slug: "paper", description: "what books are made of" },
                 ])
+            })
+    })
+})
+
+describe("GET /api/articles/article_id", () => {
+    test("200: Responds with an article object", () => {
+        return request(app)
+            .get("/api/articles/1")
+            .expect(200)
+            .then(({ body: { article } }) => {
+                expect(article).toEqual({
+                    article_id: 1,
+                    title: "Living in the shadow of a great man",
+                    topic: "mitch",
+                    author: "butter_bridge",
+                    body: "I find this existence challenging",
+                    created_at: "2020-07-09T20:11:00.000Z",
+                    votes: 100,
+                    article_img_url:
+                        "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+                })
+            })
+    })
+
+    test("400: Responds with 400 for an an invalid id", () => {
+        return request(app)
+            .get("/api/articles/a")
+            .expect(400)
+            .then((result) => {
+                expect(result.body.msg).toBe("Bad Request: Invalid id")
+            })
+    })
+
+    test("404: Responds with 404 for an id out of range", () => {
+        return request(app)
+            .get("/api/articles/9999")
+            .expect(404)
+            .then((result) => {
+                expect(result.body.msg).toBe(
+                    "No article found for specified id"
+                )
             })
     })
 })
--- app/controllers/ncNews.controller.js
+++ app/controllers/ncNews.controller.js
@@ -1,5 +1,6 @@
+const articles = require("../../db/data/test-data/articles")
 const endpoints = require("../../endpoints.json")
-const { selectTopics } = require("../models/ncNews.models")
+const { selectTopics, selectArticleFromId } = require("../models/ncNews.models")
 
 exports.getApi = (req, res, next) => {
     res.status(200).send({ endpoints })
@@ -9,4 +10,12 @@ exports.getTopics = (req, res, next) => {
     selectTopics().then((topics) => {
         res.status(200).send({ topics })
     })
+}
+
+exports.getArticleFromId = (req, res, next) => {
+    selectArticleFromId(req.params.article_id)
+        .then((article) => {
+            res.status(200).send({ article: article[0] })
+        })
+        .catch((err) => next(err))
 }
